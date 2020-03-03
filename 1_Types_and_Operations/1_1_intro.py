efdd8130a9caf2512610d81f6078c5e6f988f97c
AleChir <ale.chiruzzi@gmail.com>
2020-03-03 21:22:19 +0100

    1_1 numbers intro

--- 1_Types_and_Operations/1_1_intro.py
+++ 1_Types_and_Operations/1_1_intro.py
@@ -1,1 +1,22 @@
-#intro
+# NUMBERS ############################################################
+
+import math
+
+a = 7
+b = 2
+res1 = a + b
+res2 = a - b
+res3 = a * b
+res4 = a ** b
+res5 = a / b  # Division always returns a float
+
+print(f"{a} + {b} = {res1}")
+print(f"{a} - {b} = {res2}")
+print(f"{a} * {b} = {res3}")
+print(f"{a} ** {b} = {res4}")
+print(f"{a} / {b} = {res5}")
+
+rad = 2.5
+area = math.pi * (rad ** 2)
+print(f"area = π * (r^2) = {area} ")
+print(f"e = {math.e}")
