cb92e18f4aa3a8830c5476ca2d6973d922a5f491
AleChir <ale.chiruzzi@gmail.com>
2020-03-04 12:43:05 +0100

    mod 1_1

--- 1_Types_and_Operations/1_1_intro.py
+++ 1_Types_and_Operations/1_1_intro.py
@@ -1,6 +1,7 @@
 # NUMBERS ############################################################
 
-import math
+import math   # module for standard math operations
+import cmath  # module for operations with complex numbers
 
 a = 7
 b = 2
@@ -9,14 +10,33 @@ res2 = a - b
 res3 = a * b
 res4 = a ** b
 res5 = a / b  # Division always returns a float
+res6 = math.factorial(a)
+res7 = math.log(a, math.e)
 
 print(f"{a} + {b} = {res1}")
 print(f"{a} - {b} = {res2}")
 print(f"{a} * {b} = {res3}")
 print(f"{a} ** {b} = {res4}")
 print(f"{a} / {b} = {res5}")
+print(f"{a}! = {res6}")
+print(f"ln({a}) = {res7}")
 
 rad = 2.5
 area = math.pi * (rad ** 2)
+
 print(f"area = π * (r^2) = {area} ")
+print(f"cos(π) = {math.cos(math.pi)}")
 print(f"e = {math.e}")
+print(f"∞ = {math.inf}")
+print(f"'Not a Number' = {math.nan}")
+
+z1 = complex(1.0, 1.0)
+z1_pol = cmath.polar(z1)
+print(f"z={z1}; |z| = {z1_pol[0]} , phase(z) = {z1_pol[1]} ")
+
+'''
+For a complete list of math functions and constants read:
+https://docs.python.org/3/library/math.html
+https://docs.python.org/3/library/cmath.html
+'''
+
